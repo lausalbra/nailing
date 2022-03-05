db82be3e985e8b8355cb4084a84d9c0edcfc1d01
Jaime Ramos Lara <j23rl07@gmail.com>
2022-03-05 11:24:29 +0100

    Initial commit

---
+++ src/test/java/com/nailing/APP/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailing.APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
