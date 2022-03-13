77537ece677b73857896ae99b9cd8b24caefdd91
Daniel-Caro <danielcaroolmedo2@gmail.com>
2022-03-13 21:49:52 +0100

    Referencias a multiples botones
    Ahora se detectan las referencias a distintos botones

--- src/web-app/src/views/Cita/index.js
+++ src/web-app/src/views/Cita/index.js
@@ -7,14 +7,17 @@ export function Cita() {
         isPaneOpen: false,
         id: "",
       })
-    const myContainer = createRef();
+    
     return (
         <>
-            <button ref={myContainer} id="420" className="border-2 border-purple-600 text-black px-32 py-3 rounded-md text-1xl font-medium hover:bg-purple-600 transition duration-300"
-                onClick={() => setState({ isPaneOpen: true, id: myContainer.current.id })}>Abrir Panel</button>
+            <button id="420" className="border-2 border-purple-600 text-black px-32 py-3 rounded-md text-1xl font-medium hover:bg-purple-600 transition duration-300"
+                onClick={(event) => setState({ isPaneOpen: true, id: event.target.id })}>Abrir Panel 1</button>
+            <button id="421" className="border-2 border-purple-600 text-black px-32 py-3 rounded-md text-1xl font-medium hover:bg-purple-600 transition duration-300"
+                onClick={(event) => setState({ isPaneOpen: true, id: event.target.id })}>Abrir Panel 2</button>
             <SlidingPane title={state.id} isOpen={state.isPaneOpen} from="bottom" width="100%" onRequestClose={() => { setState({ isPaneOpen: false, id: "" });}}>
                 <PropertyPanel name="Base"></PropertyPanel>
             </SlidingPane>
         </>
     )
 }
+
