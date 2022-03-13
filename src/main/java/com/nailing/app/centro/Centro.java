6f1db4f7e1fcca53517e1e1f67b047d1b319adbe
Jacgarvel <jacobo.estudiante16@gmail.com>
2022-03-13 19:57:57 +0100

    Merge branch 'develop' into Task-041-modelo-y-repositorio-de-uña-#41

---
+++ src/main/java/com/nailing/app/centro/Centro.java
@@ -0,0 +1,131 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nailing.app.centro;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ *
+ * @author jaime
+ */
+
+@Entity
+@Table(name = "centro")
+@EntityListeners(AuditingEntityListener.class)
+public class Centro {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "nombre")
+    @Size(max=1000)
+    @NotBlank
+    private String nombre;
+    
+    @Column(name = "ubicacion")
+    @Size(max=1000)
+    @NotBlank
+    private String ubicacion;
+    
+    @Column(name = "imagen")
+    @Size(max=1000)
+    @NotBlank
+    private String imagen;
+
+    public Centro() {
+    }
+
+    public Centro(long id, String nombre, String ubicacion, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+        this.imagen = imagen;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 31 * hash + Objects.hashCode(this.nombre);
+        hash = 31 * hash + Objects.hashCode(this.ubicacion);
+        hash = 31 * hash + Objects.hashCode(this.imagen);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Centro other = (Centro) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.ubicacion, other.ubicacion)) {
+            return false;
+        }
+        return Objects.equals(this.imagen, other.imagen);
+    }
+
+    @Override
+    public String toString() {
+        return "Centro{" + "id=" + id + ", nombre=" + nombre + ", ubicacion=" + ubicacion + ", imagen=" + imagen + '}';
+    }
+    
+    
+    
+}
