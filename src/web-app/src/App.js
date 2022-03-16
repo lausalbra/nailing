c78cc630ba97105b056fa1263faaac065183f65c
ajaviermg <ajaviermorenogonzalez@gmail.com>
2022-03-16 18:39:29 +0100

    Feature: Login Usuario
    Se implementa el formulario de login de usuario a la espera de la lógica de backEnd

--- src/web-app/src/App.js
+++ src/web-app/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Header } from './components/Header'
 import { Landing } from './views/Landing'
+import { Login } from './views/Login'
 import { Route } from 'wouter'
 function App() {
   return (
+
     <div>
       <Header />
       <div className=" bg-white-100 min-h-screen grid grid-rows-[500px_300px_500px_300px]">
         {/* Las rutas entran en orden de matching por lo que la menos especifica va al final*/}
         <main className='w-4/5 mx-auto'>
+          <Route path='/login' component={Login} />
           <Route path='/' component={Landing} />
         </main>
       </div>
