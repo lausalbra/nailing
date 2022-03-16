0c363aa11bd1d902db532829c875fe79a782c21d
Jacgarvel <jacobo.estudiante16@gmail.com>
2022-03-16 18:23:03 +0100

    feat: Acabado implementado Implementado modelo, repositorio, servicio y controlador de acabado y añadida la relacion con uña. Issue #42

---
+++ src/main/java/com/nailing/app/acabado/Acabado.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nailing.app.acabado;
+
+import com.nailing.app.unya.Unya;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name="acabado")
+@EntityListeners(AuditingEntityListener.class)
+public class Acabado {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "nombre")
+    @NotBlank
+    private String nombre;
+    
+    @Column(name = "tiempo")
+    @Positive
+    private Integer tiempo;
+    
+    @Column(name = "coste")
+    @Positive
+    private Double coste;
+    
+    @OneToMany(mappedBy = "acabado")
+    private Set<Unya> unyas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getTiempo() {
+        return tiempo;
+    }
+
+    public Double getCoste() {
+        return coste;
+    }
+
+    public Set<Unya> getUnyas() {
+        return unyas;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTiempo(Integer tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public void setCoste(Double coste) {
+        this.coste = coste;
+    }
+
+    public void setUnyas(Set<Unya> unyas) {
+        this.unyas = unyas;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.nombre);
+        hash = 53 * hash + Objects.hashCode(this.tiempo);
+        hash = 53 * hash + Objects.hashCode(this.coste);
+        hash = 53 * hash + Objects.hashCode(this.unyas);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Acabado other = (Acabado) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.tiempo, other.tiempo)) {
+            return false;
+        }
+        if (!Objects.equals(this.coste, other.coste)) {
+            return false;
+        }
+        return Objects.equals(this.unyas, other.unyas);
+    }
+
+    @Override
+    public String toString() {
+        return "Acabado{" + "id=" + id + ", nombre=" + nombre + ", tiempo=" + tiempo + ", coste=" + coste + ", unyas=" + unyas + '}';
+    }
+    
+}
---
+++ src/main/java/com/nailing/app/acabado/AcabadoController.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nailing.app.acabado;
+
+import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Usuario
+ */
+@RestController
+@RequestMapping("/acab")
+public class AcabadoController {
+    
+    @Autowired
+    AcabadoService acabadoService;
+    
+        @PostMapping("/add")
+    public ResponseEntity<Acabado> addAcabado(@RequestBody Acabado acabado){
+        Acabado acab = acabadoService.addAcabado(acabado);
+        if(acab == null)
+            return new ResponseEntity<Acabado>(acab, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<Acabado>(acab, HttpStatus.CREATED);
+    }
+    
+    @DeleteMapping("/delete/{id}")
+    public void deleteAcabado(@PathVariable Long id){
+        acabadoService.removeAcabado(id);
+    }
+    
+    @GetMapping("/show/{id}")
+    public ResponseEntity<Acabado> showAcabado(@PathVariable Long id){
+        try{
+            Acabado acab = acabadoService.findById(id);
+            return new ResponseEntity<Acabado>(acab,HttpStatus.OK);
+        }catch(NoSuchElementException e){
+            return new ResponseEntity<Acabado>(acabadoService.findById(id),HttpStatus.BAD_REQUEST);
+        }       
+    }
+    
+    @GetMapping("/list")
+    public ResponseEntity<List<Acabado>> listAcabado(){
+        List<Acabado> acabados = StreamSupport.stream(acabadoService.findAll().spliterator(), false).collect(Collectors.toList());
+        return new ResponseEntity<List<Acabado>>(acabados,HttpStatus.OK);
+    }
+}
