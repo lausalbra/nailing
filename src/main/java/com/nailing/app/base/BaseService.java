64770832a48f8e568c5eaad577a35ff9eacb4802
Hegoa <hegoaria@gmail.com>
2022-03-19 19:33:10 +0100

    feat: Completada la task, creado el endpoint donde se consultan las bases a elegir Modificada la entidad Base para que concuerde con el UML. Añadido el endpoint y su lógica de negocio para obtener las bases disponibles después de elegir un tipo en el proceso de creación de cita. Creado el Enum de fases. Issue #43

--- src/main/java/com/nailing/app/base/BaseService.java
+++ src/main/java/com/nailing/app/base/BaseService.java
@@ -1,17 +1,23 @@
 package com.nailing.app.base;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.nailing.app.centro.CentroRepository;
+import com.nailing.app.tipo.Tipo;
+import com.nailing.app.tipo.TipoRepository;
 
 @Service("baseService")
 public class BaseService {
 
 	@Autowired
 	private BaseRepository baseRepository;
+	@Autowired
+	private TipoRepository tipoRepository;
 	
 //	guardar/editar
 	public Base addBase(Base base) {
@@ -34,5 +40,37 @@ public class BaseService {
 		Base base = findById(id);
 		if(base!=null)
 			baseRepository.delete(base);
+	}
+	
+//	encontrar las bases que posee el centro dado según el tipo seleccionado
+	public List<Base> findBasesByCentroTipo(Long tipoId, Long centroId) {
+		Optional<Tipo> tipo = tipoRepository.findById(tipoId);
+		List<Base> result = new ArrayList<>();
+		List<NombreBase> bases;
+		List<Base> basesCentro;
+		
+		switch (tipo.get().getNombre()) {
+		case "ESCULPIDA":
+			bases = Arrays.asList(NombreBase.DUAL_SYSTEM, NombreBase.GEL, NombreBase.ACRILICO, NombreBase.ACRYGEL);
+			basesCentro = baseRepository.findByCentro(centroId);
+			
+			for(Base b: basesCentro) {
+				if(bases.contains(b.getNombre()))
+					result.add(b);
+			}
+			break;
+
+		case "NATURALES":
+			bases = Arrays.asList(NombreBase.SEMIPERMANENTE, NombreBase.SEMIPERMANENTE_REFUERZO, NombreBase.JAPONESA);
+			basesCentro = baseRepository.findByCentro(centroId);
+			
+			for(Base b: basesCentro) {
+				if(bases.contains(b.getNombre()))
+					result.add(b);
+			}
+			break;
+		}
+		
+		return result;
 	}
 }
