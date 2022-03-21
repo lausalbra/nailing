897ed497f0e7d4cf8eb9ed449013f069cb84ab1a
ajaviermg <ajaviermorenogonzalez@gmail.com>
2022-03-21 17:23:33 +0100

    Merge Detalles

--- src/web-app/src/App.js
+++ src/web-app/src/App.js
@@ -5,7 +5,7 @@ import { Feedback } from './views/Feedback'
 import { Login } from './views/Login'
 import { Route } from 'wouter'
 import { BookingsList } from './views/BookingsList'
-// import { Usuario } from './views/Usuario'
+import { Usuario } from './views/Usuario'
 import { Conocenos } from './views/Conocenos'
 import { Cita } from './views/Cita'
 import { MisCitasList } from './views/MisCitasList'
@@ -18,7 +18,7 @@ function App() {
         {/* Las rutas entran en orden de matching por lo que la menos especifica va al final */}
         <div className='w-4/5 bg-white mx-auto min-h-screen shadow-2xl shadow-pink-300 '>
           <Route path='/login' component={Login} />
-          {/* <Route path='/usuario' component={Usuario} /> */}
+          <Route path='/usuario' component={Usuario} />
           <Route path='/centro' component={BookingsList} />
           <Route path='/conocenos' component={Conocenos} />
           <Route path='/cita' component={BookingsList} />
