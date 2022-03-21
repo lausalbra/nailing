d3482a6d41a77051f7696fd59bd3edd4e2fc9cee
vicente <vicsorvaz@alum.us.es>
2022-03-21 10:21:44 +0100

    feat: creada vista /miscitas con el listado de citas del usuario

--- src/web-app/src/App.js
+++ src/web-app/src/App.js
@@ -5,22 +5,23 @@ import { Login } from './views/Login'
 import { Route } from 'wouter'
 import { BookingsList } from './views/BookingsList'
 import { Usuario } from './views/Usuario'
-import { Centro } from './views/Centro'
 import { Conocenos } from './views/Conocenos'
 import { Cita } from './views/Cita'
-function App() {
+import { MisCitasList } from './views/MisCitasList'
+function App () {
   return (
 
     <div className='overflow-auto'>
       <Header />
-      <div className=" bg-slate-100 ">
-        {/* Las rutas entran en orden de matching por lo que la menos especifica va al final*/}
+      <div className=' bg-slate-100 '>
+        {/* Las rutas entran en orden de matching por lo que la menos especifica va al final */}
         <div className='w-4/5 bg-white mx-auto min-h-screen shadow-2xl shadow-pink-300 '>
           <Route path='/login' component={Login} />
           <Route path='/usuario' component={Usuario} />
-          <Route path='/centro' component={Centro} />
+          <Route path='/centro' component={BookingsList} />
           <Route path='/conocenos' component={Conocenos} />
           <Route path='/cita' component={BookingsList} />
+          <Route path='/miscitas' component={MisCitasList} />
           <Route path='/personalizacion' component={Cita} />
           <Route path='/' component={Landing} />
         </div>
--- src/web-app/src/components/Header/index.js
+++ src/web-app/src/components/Header/index.js
@@ -1,15 +1,14 @@
-import { Link } from "wouter"
-export function Header() {
-
-    return (
-        <>
-            <nav className='flex justify-evenly items-center bg-logo-completo bg-white border-pink-200 border-b-2 bg-no-repeat bg-center  text-black sticky top-0 z-10'>
-                <Link className="hover:text-red-400" to="/conocenos">Conócenos</Link>
-                <Link className="hover:text-red-400" to="/feedback">Feedback</Link>
-                <Link className="block w-64 h-20" to="/"></Link>
-                <Link className="hover:text-red-400" to="/centros">Centros</Link>
-                <Link className="hover:text-red-400" to="/login">Iniciar Sesion</Link>
-            </nav>
-        </>)
-
+import { Link } from 'wouter'
+export function Header () {
+  return (
+    <>
+      <nav className='flex justify-evenly items-center bg-logo-completo bg-white border-pink-200 border-b-2 bg-no-repeat bg-center  text-black sticky top-0 z-10'>
+        <Link className='hover:text-red-400' to='/conocenos'>Conócenos</Link>
+        <Link className='hover:text-red-400' to='/feedback'>Feedback</Link>
+        <Link className='block w-64 h-20' to='/' />
+        <Link className='hover:text-red-400' to='/centro'>Centros</Link>
+        <Link className='hover:text-red-400' to='/login'>Iniciar Sesion</Link>
+      </nav>
+    </>
+  )
 }
