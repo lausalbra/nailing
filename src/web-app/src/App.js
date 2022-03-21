e13c092311b3ebd522244278225c4f17e1dc4d7e
ajaviermg <ajaviermorenogonzalez@gmail.com>
2022-03-21 17:16:07 +0100

    Merge Listado Centros y Formulario Feedback

--- src/web-app/src/App.js
+++ src/web-app/src/App.js
@@ -1,18 +1,15 @@
 import './App.css'
 import { Header } from './components/Header'
 import { Landing } from './views/Landing'
-<<<<<<< Updated upstream
-=======
 import { Feedback } from './views/Feedback'
 import { Login } from './views/Login'
->>>>>>> Stashed changes
 import { Route } from 'wouter'
 import { BookingsList } from './views/BookingsList'
-import { Usuario } from './views/Usuario'
+// import { Usuario } from './views/Usuario'
 import { Conocenos } from './views/Conocenos'
 import { Cita } from './views/Cita'
 import { MisCitasList } from './views/MisCitasList'
-function App () {
+function App() {
   return (
 
     <div className='overflow-auto'>
@@ -21,7 +18,7 @@ function App () {
         {/* Las rutas entran en orden de matching por lo que la menos especifica va al final */}
         <div className='w-4/5 bg-white mx-auto min-h-screen shadow-2xl shadow-pink-300 '>
           <Route path='/login' component={Login} />
-          <Route path='/usuario' component={Usuario} />
+          {/* <Route path='/usuario' component={Usuario} /> */}
           <Route path='/centro' component={BookingsList} />
           <Route path='/conocenos' component={Conocenos} />
           <Route path='/cita' component={BookingsList} />
