7ee41023e454b4e099541e7da8daf0419a52c730
Jaime Ramos Lara <j23rl07@gmail.com>
2022-03-22 18:49:39 +0100

    fix: cors

--- src/main/java/com/nailing/app/securityConfiguration/SecurityConfiguration.java
+++ src/main/java/com/nailing/app/securityConfiguration/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().authorizeRequests()
+        http.csrf().disable().cors().and().authorizeRequests()
             //.antMatchers("/login").permitAll()
             //.antMatchers("/**").authenticated()
             //.antMatchers("/**/**").authenticated()
