bf5a3d4d5772eee9b9f2b3e9e765cf998f4f1278
ajaviermg <ajaviermorenogonzalez@gmail.com>
2022-03-23 17:59:14 +0100

    Fix: Formulario Login con llamadas
    - Se añaden llamadas al endpoint nailingDevelop (puede cambiar)
    - Se establece el funcionamiento esperado, sin probar

--- src/web-app/src/components/LoginForm/index.js
+++ src/web-app/src/components/LoginForm/index.js
@@ -1,31 +1,64 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import { useLocation } from 'wouter'
+import { httpRequest } from '../../services/common/commonService'
 
 export function LoginForm() {
 
+    const [state, changeState] = useState(true)
+    const [locationPath, locationPush] = useLocation()
     const user = useRef()
     const password = useRef()
 
     function handleSubmit(evt) {
         evt.preventDefault()
+        const url = "https://nailingdevelop.herokuapp.com/login"
+        const body = {
+            "user": user,
+            "password": password
+        }
+        const userLogged = httpRequest('POST', url, [], body)
 
-        if (user.current.value === "user" && password.current.value === "password") {
-            sessionStorage.setItem("userLogged", true);
+
+        if (userLogged.getResponseHeader('status') === 200) {
+            //Añado el id y el estado isLogged
+            sessionStorage.setItem("userLogged", userLogged);
+            sessionStorage.setItem("isLogged", true);
+
+            //Aqui hago get del usuario para obtener sus datos
+            const url = `https://nailingdevelop.herokuapp.com/usuarios/${userLogged}`
+            // Cabezera authorization generada con https://www.blitter.se/utils/basic-authentication-header-generator/ y datos usuario1 usuario1
+            const data = httpRequest('GET', url, ["Authorization: Basic dXN1YXJpbzE6dXN1YXJpbzE="], "")
+            sessionStorage.setItem("userName", data.nombre);
+            sessionStorage.setItem("userEmail", data.email);
+            sessionStorage.setItem("userPhone", data.telefono);
+            changeState(true)
+            locationPush('/')
         } else {
-            sessionStorage.setItem("userLogged", false);
+            changeState(false)
+
         }
         console.log(user.current.value, password.current.value)
     }
 
-    return (
-        <form className='grid border-2 border-red-300 p-5 rounded-md' onSubmit={handleSubmit} >
-            <label htmlFor="user"> Usuario:</label>
-
-            <input name="user" className="border-black border-2 mb-4" type="text" ref={user} />
-            <label htmlFor="password"> Contraseña:</label>
-            <input name="password" className="border-black border-2 mb-4" type="password" ref={password} />
-            <input className="border-black border-2 mb-4 cursor-pointer" type="submit" value="Enviar" />
-
+    return state ? (
+        <form className='grid border-2 border-pink-300 p-5 rounded-md' onSubmit={handleSubmit} >
+            <label className='text-lg' htmlFor="user"> Usuario:</label>
+            <input className="border-black border-2  rounded-sm mb-4" name="user" type="text" ref={user} />
+            <label className='text-lg' htmlFor="password">   Contraseña:</label>
+            <input className="border-black border-2 mb-4 rounded-sm" name="password" type="password" ref={password} />
+            <input className="border-black border-2 mb-4 cursor-pointer hover:bg-pink-200 hover:border-pink-200 duration-300 rounded-3xl" type="submit" value="Enviar" />
         </form>
     )
+        :
+        (<form className='grid border-2 border-pink-300 p-5 rounded-md' onSubmit={handleSubmit} >
+            <label className='text-lg' htmlFor="user"> Usuario:</label>
+
+            <input className="border-black border-2  rounded-sm mb-4" name="user" type="text" ref={user} required minLength={1} maxLength={100} />
+            <label className='text-lg' htmlFor="password">   Contraseña:</label>
+            <input className="border-black border-2 mb-4 rounded-sm" name="password" type="password" ref={password} required minLength={8} maxLength={100} />
+            < p className='text-sm text-red-600 pb-2' > Usuario o Contraseña No Válidos</p>
+            <input className="border-black border-2 mb-4 cursor-pointer hover:bg-pink-200 hover:border-pink-200 duration-300 rounded-3xl" type="submit" value="Enviar" />
+
+        </form >)
 
 }
