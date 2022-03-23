7388f125699532a71ae316ebed65f652919bd469
vicente <vicsorvaz@alum.us.es>
2022-03-23 19:48:37 +0100

    feat: Bugfixes en vistas centros

--- src/web-app/src/views/Centro/index.js
+++ src/web-app/src/views/Centro/index.js
@@ -1,18 +1,16 @@
-import { CenterDetails } from '../../components/CenterDetails';
-import { Nombre } from '../../components/Nombre';
+import { CenterDetails } from '../../components/CenterDetails'
+import { Nombre } from '../../components/Nombre'
 import foto from '../../static/Logo-Inicial.png'
-import { Header } from "../../components/Header"
+import { Header } from '../../components/Header'
 
-export function Centro() {
-    return (
-        <>
-            <Header />
-            <Nombre name={'Name'} ></Nombre>
-            <CenterDetails image={foto} address={'C/calle nº1'} rating={'3'}  ></CenterDetails>
+export function Centro () {
+  return (
+    <>
+      <Header />
+      <Nombre name='Name' />
+      <CenterDetails image={foto} address='C/calle nº1' rating='3' />
 
-        </>
+    </>
 
-
-
-    )
+  )
 }
