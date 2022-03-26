deb23dddf00404d40d35e6d21fdc74901199805b
vicente <vicsorvaz@alum.us.es>
2022-03-26 13:13:10 +0100

    test: prueba de test

--- src/web-app/src/App.test.js
+++ src/web-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText("Centros");
+  const linkElement = screen.getByText("Conócenos");
   expect(linkElement).toBeInTheDocument();
 });
