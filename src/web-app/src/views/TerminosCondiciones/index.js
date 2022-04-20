6abe92e683109ebd3061d44067c558ee0a76b698
Daniel-Caro <61906384+Daniel-Caro@users.noreply.github.com>
2022-04-20 14:00:07 +0200

    feat/Datos de términos y condiciones
    Se ha rellenado toda la información de términos y condiciones.

--- src/web-app/src/views/TerminosCondiciones/index.js
+++ src/web-app/src/views/TerminosCondiciones/index.js
@@ -5,9 +5,69 @@ export function TerminosCondiciones() {
     return (
         <>
             <Header />
-            <h1 className='p-5 text-2xl underline'><i>Terminos y Condiciones</i></h1>
+            <h1 className='p-2 text-2xl underline'><i>Terminos y Condiciones</i></h1>
             <div>
-                
+                <h2 className='p-3 text-xl underline'><i>Principios aplicados en el tratamiento de datos</i></h2>
+                <p className='p-5 text-sm '>En el tratamiento de tus datos personales, el Titular aplicará los siguientes principios que se ajustan a las exigencias del nuevo reglamento europeo de protección de datos:
+                <br></br>Principio de licitud, lealtad y transparencia: El Titular siempre requerirá el consentimiento para el tratamiento de tus datos personales que puede ser para uno o varios fines específicos sobre los que te informará previamente con absoluta transparencia.
+                <br></br>Principio de minimización de datos: El Titular te solicitará solo los datos estrictamente necesarios para el fin o los fines que los solicita.
+                <br></br>Principio de limitación del plazo de conservación: Los datos se mantendrán durante el tiempo estrictamente necesario para el fin o los fines del tratamiento.
+                <br></br>El Titular te informará del plazo de conservación correspondiente según la finalidad. En el caso de suscripciones, el Titular revisará periódicamente las listas y eliminará aquellos registros inactivos durante un tiempo considerable.
+                <br></br>Principio de integridad y confidencialidad: Tus datos serán tratados de tal manera que su seguridad, confidencialidad e integridad esté garantizada. Debes saber que el Titular toma las precauciones necesarias para evitar el acceso no autorizado o uso indebido de los datos de sus usuarios por parte de terceros.</p>
+                <h2 className='p-3 text-xl underline'><i>Obtención de datos personales</i></h2>
+                <p className='p-5 text-sm '>Para navegar por Nailing no es necesario que facilites ningún dato personal. Los casos en los que sí proporcionas tus datos personales son los siguientes:
+                <br></br>Al registrarte como usuario en nuestra aplicación</p>
+                <h2 className='p-3 text-xl underline'><i>Tus derechos</i></h2>
+                <p className='p-5 text-sm '>El Titular te informa que sobre tus datos personales tienes derecho a:
+                <br></br>Solicitar el acceso a los datos almacenados.
+                <br></br>Solicitar una rectificación o la cancelación.
+                <br></br>Solicitar la limitación de su tratamiento.
+                <br></br>Oponerte al tratamiento.
+                <br></br>Solicitar la portabilidad de tus datos.
+                <br></br>El ejercicio de estos derechos es personal y por tanto debe ser ejercido directamente por el interesado, solicitándolo directamente al Titular, lo que significa que cualquier cliente, suscriptor o colaborador que haya facilitado sus datos en algún momento puede dirigirse al Titular y pedir información sobre los datos que tiene almacenados y cómo los ha obtenido, solicitar la rectificación de los mismos, solicitar la portabilidad de sus datos personales, oponerse al tratamiento, limitar su uso o solicitar la cancelación de esos datos en los ficheros del Titular.
+                <br></br>
+                <br></br>Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a nailing.app@gmail.com junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.
+                <br></br>Tienes derecho a la tutela judicial efectiva y a presentar una reclamación ante la autoridad de control, en este caso, la Agencia Española de Protección de Datos, si consideras que el tratamiento de datos personales que te conciernen infringe el Reglamento.</p>
+                <h2 className='p-3 text-xl underline'><i>Finalidad del tratamiento de datos personales</i></h2>
+                <p className='p-5 text-sm '>Cuando te conectas al sitio Web para mandar un correo al Titular, te suscribes a su boletín o realizas una contratación, estás facilitando información de carácter personal de la que el responsable es el Titular. Esta información puede incluir datos de carácter personal como pueden ser tu dirección IP, nombre y apellidos, dirección física, dirección de correo electrónico, número de teléfono, y otra información. Al facilitar esta información, das tu consentimiento para que tu información sea recopilada, utilizada, gestionada y almacenada por Heroku, sólo como se describe en el Aviso Legal y en la presente Política de Privacidad.
+                <br></br>Los datos personales y la finalidad del tratamiento por parte del Titular son diferente según el sistema de captura de información:
+                <br></br>Formularios de feedback: El Titular solicita datos personales entre los que pueden estar: Nombre y apellidos, dirección de correo electrónico, número de teléfono con la finalidad de responder a tus consultas.
+                <br></br>Por ejemplo, el Titular utiliza esos datos para dar respuesta a tus mensajes, dudas, quejas, comentarios o inquietudes que puedas tener relativas a la información incluida en el sitio Web, los servicios que se prestan a través del sitio Web, el tratamiento de tus datos personales, cuestiones referentes a los textos legales incluidos en el sitio Web, así como cualquier otra consulta que puedas tener y que no esté sujeta a las condiciones del sitio Web o de la contratación.
+                <br></br>Formularios de suscripción a contenidos: El Titular solicita los siguientes datos personales: Nombre y apellidos, dirección de correo electrónico, número de teléfono y dirección de tu local para gestionar la lista de suscripciones.
+                <br></br>Existen otras finalidades por las que el Titular trata tus datos personales:
+                <br></br>Para garantizar el cumplimiento de las condiciones recogidas en el Aviso Legal y en la ley aplicable. Esto puede incluir el desarrollo de herramientas y algoritmos que ayuden a este sitio Web a garantizar la confidencialidad de los datos personales que recoge.
+                <br></br>Para apoyar y mejorar los servicios que ofrece este sitio Web.
+                <br></br>Para gestionar las redes sociales. el Titular tiene presencia en redes sociales. Si te haces seguidor en las redes sociales del Titular el tratamiento de los datos personales se regirá por este apartado, así como por aquellas condiciones de uso, políticas de privacidad y normativas de acceso que pertenezcan a la red social que proceda en cada caso y que has aceptado previamente.
+                <br></br>Puedes consultar las políticas de privacidad de las principales redes sociales en estos enlaces:
+                <br></br>Twitter: @nailing_app
+                <br></br>Instagram: @nailing_app
+                <br></br>El Titular tratará tus datos personales con la finalidad de administrar correctamente su presencia en la red social, informarte de sus actividades, productos o servicios, así como para cualquier otra finalidad que las normativas de las redes sociales permitan.
+                <br></br>
+                <br></br>En ningún caso el Titular utilizará los perfiles de seguidores en redes sociales para enviar publicidad de manera individual.</p>
+                <h2 className='p-3 text-2xl underline'><i>Seguridad de los datos personales</i></h2>
+                <p className='p-5 text-sm '>Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o destrucción de estos.<br></br>El sitio Web está alojado en Heroku. La seguridad de tus datos está garantizada, ya que toman todas las medidas de seguridad necesarias para ello. Puedes consultar su política de privacidad para tener más información.</p>
+                <h2 className='p-3 text-xl underline'><i>Contenido de otros sitios web</i></h2>
+                <p className='p-5 text-sm '>Las páginas de este sitio Web pueden incluir contenido incrustado (por ejemplo, vídeos, imágenes, artículos, etc.). El contenido incrustado de otras web se comporta exactamente de la misma manera que si hubieras visitado la otra web.<br></br>Estos sitios Web pueden recopilar datos sobre ti, utilizar cookies, incrustar un código de seguimiento adicional de terceros, y supervisar tu interacción usando este código.</p>
+                <h2 className='p-3 text-xl underline'><i>Política de Cookies</i></h2>
+                <p className='p-5 text-sm '>Para que este sitio Web funcione correctamente necesita utilizar cookies, que es una información que se almacena en tu navegador web.<br></br>En la página Política de Cookies puedes consultar toda la información relativa a la política de recogida, la finalidad y el tratamiento de las cookies.</p>
+                <h2 className='p-3 text-xl underline'><i>Legitimación para el tratamiento de datos</i></h2>
+                <p className='p-5 text-sm '>La base legal para el tratamiento de tus datos es: el consentimiento.<br></br>Para contactar con el Titular, suscribirte a un boletín o realizar comentarios en este sitio Web tienes que aceptar la presente Política de Privacidad.</p>
+                <h2 className='p-3 text-xl underline'><i>Categorías de datos personales</i></h2>
+                <p className='p-5 text-sm '>Las categorías de datos personales que trata el Titular son: <br></br>Datos identificativos.</p>
+                <h2 className='p-3 text-xl underline'><i>Conservación de datos personales</i></h2>
+                <p className='p-5 text-sm '>Los datos personales que proporciones al Titular se conservarán hasta que solicites su supresión.</p>
+                <h2 className='p-3 text-xl underline'><i>Navegación Web</i></h2>
+                <p className='p-5 text-sm '>Al navegar por Nailing se pueden recoger datos no identificativos, que pueden incluir, la dirección IP, geolocalización, un registro de cómo se utilizan los servicios y sitios, hábitos de navegación y otros datos que no pueden ser utilizados para identificarte. <br></br>El Titular utiliza la información obtenida para obtener datos estadísticos, analizar tendencias, administrar el sitio, estudiar patrones de navegación y para recopilar información demográfica.</p>
+                <h2 className='p-3 text-xl underline'><i>Exactitud y veracidad de los datos personales</i></h2>
+                <p className='p-5 text-sm '>Te comprometes a que los datos facilitados al Titular sean correctos, completos, exactos y vigentes, así como a mantenerlos debidamente actualizados. <br></br>Como Usuario del sitio Web eres el único responsable de la veracidad y corrección de los datos que remitas al sitio exonerando a el Titular de cualquier responsabilidad al respecto.</p>
+                <h2 className='p-3 text-xl underline'><i>Aceptación y consentimiento</i></h2>
+                <p className='p-5 text-sm '>Como Usuario del sitio Web declaras haber sido informado de las condiciones sobre protección de datos de carácter personal, aceptas y consientes el tratamiento de estos por parte del Titular en la forma y para las finalidades indicadas en esta Política de Privacidad.</p>
+                <h2 className='p-3 text-xl underline'><i>Revocabilidad</i></h2>
+                <p className='p-5 text-sm '>Para ejercitar tus derechos de acceso, rectificación, cancelación, portabilidad y oposición tienes que enviar un correo electrónico a nailing.app@gmail.com junto con la prueba válida en derecho como una fotocopia del D.N.I. o equivalente.<br></br> El ejercicio de tus derechos no incluye ningún dato que el Titular esté obligado a conservar con fines administrativos, legales o de seguridad.</p>
+                <h2 className='p-3 text-xl underline'><i>Cambios en la Política de Privacidad</i></h2>
+                <p className='p-5 text-sm '>El Titular se reserva el derecho a modificar la presente Política de 
+                Privacidad para adaptarla a novedades legislativas o jurisprudenciales, así como a prácticas de la 
+                industria.<br></br> Estas políticas estarán vigentes hasta que sean modificadas por otras debidamente publicadas.</p>
             </div>
 
         </>
