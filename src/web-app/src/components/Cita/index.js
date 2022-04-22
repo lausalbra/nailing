fd97907bf61099b6f64ccdc68c7e9ce61227cbe5
Antonio González Gómez <antoniogg696@gmail.com>
2022-04-22 14:24:04 +0200

    fix: Unificación de colores
    Se ha cambiado la paleta de colores usada en varios componentes

--- src/web-app/src/components/Cita/index.js
+++ src/web-app/src/components/Cita/index.js
@@ -10,8 +10,8 @@ export function Cita ({ cita, deleteFunc }) {
     }
 
     return (
-        <Box style={{backgroundColor: 'rgb(248, 225, 228)'}}
-            sx={{ width: '88%', flexDirection: 'row', mx:'1%', my:2, display: 'inline-flex', alignItems: 'center', justifyContent: 'space-around', border:2, borderRadius: 2, borderColor: '#F39EEC' }}>
+        <Box style={{background: 'linear-gradient(145deg, rgba(255,226,239,1) 5%, rgba(235,190,210,1) 100%)'}}
+            sx={{ width: '88%', flexDirection: 'row', mx:'1%', my:2, display: 'inline-flex', alignItems: 'center', justifyContent: 'space-around', border:2, borderRadius: 2, borderColor: '#e0b4c7' }}>
             <CardContent sx={{ flexGrow: 1, width: '49%'}}>
                 <div className="flex items-center">
                     <img src={cita.centro.imagen} alt={cita.centro.nombre} className="object-cover rounded-md shadow-md max-w-full float-left bg-white" />
@@ -21,7 +21,7 @@ export function Cita ({ cita, deleteFunc }) {
                 <p className="m-1 text-center"><strong>{cita.centro.nombre}</strong></p>
                 <p className="m-1 text-center">{dateParse(cita.horaInicio)}</p>
                 <p className="m-1 text-center">Coste: {cita.coste} €</p>
-                <button id="1" className="border-2 border-purple-400 bg-pink-200 text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-purple-300 transition duration-300"
+                <button id="1" className="border-2 border-[#e0b4c7] bg-[#ffe2ef] text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-[#d9ebff] hover:border-[#abc2db] transition duration-300"
                     onClick={deleteFunc}>Cancelar cita</button>
             </Box>
         </Box>
