1fa05e5de78cbe7d84caffa69818e70817ea3484
Antonio González Gómez <antoniogg696@gmail.com>
2022-04-24 13:19:57 +0200

    fix: Implementar responsive
    Se han reestructurado algunos componentes para que su tamaño y disposición sea apropiado para dispositivos móviles.

--- src/web-app/src/components/Cita/index.js
+++ src/web-app/src/components/Cita/index.js
@@ -12,16 +12,16 @@ export function Cita ({ cita, deleteFunc }) {
     return (
         <Box style={{background: 'linear-gradient(145deg, rgba(255,226,239,1) 5%, rgba(235,190,210,1) 100%)'}}
             sx={{ width: '88%', flexDirection: 'row', mx:'1%', my:2, display: 'inline-flex', alignItems: 'center', justifyContent: 'space-around', border:2, borderRadius: 2, borderColor: '#e0b4c7' }}>
-            <CardContent sx={{ flexGrow: 1, width: '49%'}}>
+            <CardContent sx={{ flexGrow: 1, width: '49%', p:'1%'}}>
                 <div className="flex items-center">
-                    <img src={cita.centro.imagen} alt={cita.centro.nombre} className="object-cover rounded-md shadow-md max-w-full float-left bg-white" />
+                    <img src={cita.centro.imagen} alt={cita.centro.nombre} className="object-cover mx-auto rounded-[20%] shadow-md w-[100%] md:w-[80%] lg:w-[60%] float-left bg-white" />
                 </div>
             </CardContent>
             <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '49%'}}>
-                <p className="m-1 text-center"><strong>{cita.centro.nombre}</strong></p>
-                <p className="m-1 text-center">{dateParse(cita.horaInicio)}</p>
-                <p className="m-1 text-center">Coste: {cita.coste} €</p>
-                <button id="1" className="border-2 border-[#e0b4c7] bg-[#ffe2ef] text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-[#d9ebff] hover:border-[#abc2db] transition duration-300"
+                <p className="m-2 text-lg text-center"><strong>{cita.centro.nombre}</strong></p>
+                <p className="m-2 text-lg text-center">{dateParse(cita.horaInicio)}</p>
+                <p className="m-2 text-lg text-center">Precio: {cita.coste} €</p>
+                <button id="1" className="border-2 border-[#e0b4c7] bg-[#ffe2ef] text-lg text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-[#d9ebff] hover:border-[#abc2db] transition duration-300"
                     onClick={deleteFunc}>Cancelar cita</button>
             </Box>
         </Box>
--- src/web-app/src/components/ServiciosBox/index.js
+++ src/web-app/src/components/ServiciosBox/index.js
@@ -4,7 +4,7 @@ export function ServiciosBox({ title, pers}) {
     return (
         <Box style={{background: 'linear-gradient(145deg, rgba(255,226,239,1) 5%, rgba(235,190,210,1) 100%)'}}
             sx={{ width: '98%', flexDirection: 'column', mx:'1%', my:'2%', display: 'inline-flex', alignItems: 'center', justifyContent: 'space-around', border:2, borderRadius: 2, borderColor: '#e0b4c7' }}>
-            <h3 className="text-3xl font-bold underline underline-offset-2">{title}</h3>
+            <h3 className="text-xl font-bold">{title}</h3>
             <Box disableSpacing sx={{flexDirection: 'column', display: 'flex', width: '100%'}}>
                 {pers}
             </Box>
