48d02bd94dd8318c0f9b599d519250cffaef6a2b
davbarsal1 <72515663+davbarsal1@users.noreply.github.com>
2022-04-25 14:11:00 +0200

    test: Finalizados los tests para Tamanyo y Forma

--- src/test/java/com/nailing/APP/service/FormaTestService.java
+++ src/test/java/com/nailing/APP/service/FormaTestService.java
@@ -5,16 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 import javax.validation.ConstraintViolationException;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.Validator;
 
 import com.nailing.app.AppApplication;
 import com.nailing.app.centro.Centro;
@@ -37,6 +43,8 @@ public class FormaTestService {
 	private Forma forma;
 	private Centro centro;
 	private Centro centroAnadido;
+	@Autowired
+	Validator validator;
 	@BeforeEach
 	public void setUp() {
 		centro = new Centro();
@@ -71,16 +79,23 @@ public class FormaTestService {
 		assertEquals(forma.getSiguienteFase(), formaAnadida.getSiguienteFase());
 		assertEquals(forma.getTiempo(), formaAnadida.getTiempo());
 	}
-	//Se comprueba que los datos seteados previamente con los datos de la forma una vez añadida a la base de datos
+	//Se comprueba que si se introduce un atributo nulo, de un error de validación
 	@Test
-	public void addFormaNull() {
+	public void addFormaNullTest() {
 		forma = new Forma();
 		forma.setCentro(centroAnadido);
 		forma.setCoste(20.0);
+		forma.setTiempo(null);
 		forma.setNombre(NombreForma.STILETTO);
 		forma.setSiguienteFase(Fases.tamanyos);
-		assertThrows(ConstraintViolationException.class, formaSer.addForma(forma));
-	}
+		assertThrows(ConstraintViolationException.class, new Executable() {
+            
+            public void execute() throws Throwable {
+                formaSer.addForma(forma);
+            }
+        });
+    }
+	
 	//Se comprueba que los datos de la forma que se obtiene son los mismos que los datos de la forma recientemente añadida
 	@Test
 	public void findByIdTest() {
@@ -93,7 +108,24 @@ public class FormaTestService {
 		Forma formaAnadida = formaSer.addForma(forma);
 		Forma formaObtenida = formaSer.findById(formaAnadida.getId());
 		assertEquals(formaObtenida.getId(), formaAnadida.getId());
-	
+	}
+	//Se comprueba que si se le pasa una id erronea da un error de validacion
+	@Test
+	public void notFindByIdTest() {
+		forma = new Forma();
+		forma.setCentro(centroAnadido);
+		forma.setCoste(20.0);
+		forma.setNombre(NombreForma.STILETTO);
+		forma.setSiguienteFase(Fases.tamanyos);
+		forma.setTiempo(30);
+		Forma formaAnadida = formaSer.addForma(forma);
+		assertThrows(NoSuchElementException.class, new Executable() {
+            
+            public void execute() throws Throwable {
+            	formaSer.findById(formaAnadida.getId()+1);
+            }
+        });
+		
 	}
 	//Se comprueba que una vez añadida la nueva forma esta se lista junto con las otras
 	@Test
@@ -160,5 +192,52 @@ public class FormaTestService {
 		Forma formaAnadida = formaSer.addForma(forma);
 		this.formaSer.removeFormabyCentro(centroAnadido.getId());
 		assertFalse(formaSer.findAll().contains(formaAnadida));
+	}
+	//Se comprueba que se ha añadido una forma a un centro de manera correcta
+	@Test
+	public void addFormaCentroTest() {
+		Map<String, List<String>> datos = new HashMap<String, List<String>>();
+		String centroKey = "centro";
+		List<String> centroValue = new ArrayList<String>();
+		centroValue.add(centroAnadido.getId().toString());
+		String costeKey = "coste";
+		List<String> costeValue = new ArrayList<String>();
+		costeValue.add("50");
+		String duracionKey = "tiempo";
+		List<String> duracionValue = new ArrayList<String>();
+		duracionValue.add("5");
+		String persoKey = "personalizaciones";
+		List<String> persoValue = new ArrayList<String>();
+		persoValue.add("SQUARE");
+		datos.put(centroKey, centroValue); datos.put(costeKey, costeValue);datos.put(duracionKey, duracionValue); datos.put(persoKey, persoValue);
+		List<Forma> formasAnadidas = formaSer.addFormaCentro(datos);
+		assertEquals(centroSer.findById(centroAnadido.getId()).get(), formasAnadidas.get(0).getCentro());
+	}
+	//Se comprueba que la falta de datos provoca una excepcion a la hora de añadir una forma a un centro
+	@Test
+	public void addFormaCentroNullTest() {
+		Map<String, List<String>> datos = new HashMap<String, List<String>>();
+		String centroKey = "centro";
+		List<String> centroValue = null;
+		String costeKey = "coste";
+		List<String> costeValue = new ArrayList<String>();
+		costeValue.add("50");
+		String duracionKey = "tiempo";
+		List<String> duracionValue = new ArrayList<String>();
+		duracionValue.add("5");
+		String persoKey = "personalizaciones";
+		List<String> persoValue = new ArrayList<String>();
+		persoValue.add("SQUARE");
+		datos.put(centroKey, centroValue); datos.put(costeKey, costeValue);datos.put(duracionKey, duracionValue); datos.put(persoKey, persoValue); 
+		assertThrows(IllegalArgumentException.class, new Executable() {
+            
+            public void execute() throws Throwable {
+            	formaSer.addFormaCentro(datos);
+            }
+        });
+	}
+	@AfterEach
+	public void deleteData() {
+		centroSer.delete(centroAnadido.getId());
 	}
 }
--- src/test/java/com/nailing/APP/service/TamayoTestService.java
+++ src/test/java/com/nailing/APP/service/TamayoTestService.java
@@ -5,12 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import javax.validation.ConstraintViolationException;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.nailing.app.AppApplication;
@@ -70,6 +77,22 @@ public class TamayoTestService {
 		assertEquals(tamanyo.getSiguienteFase(), tamanyoAnadido.getSiguienteFase());
 		assertEquals(tamanyo.getTiempo(), tamanyoAnadido.getTiempo());
 	}
+	//Se comprueba que si se introduce un atributo nulo, de un error de validación
+	@Test
+	public void addFormaNullTest() {
+		tamanyo = new Tamanyo();
+		tamanyo.setCentro(centroAnadido);
+		tamanyo.setCoste(20.0);
+		tamanyo.setNombre(null);
+		tamanyo.setSiguienteFase(Fases.disenyos);
+		tamanyo.setTiempo(30);
+		assertThrows(ConstraintViolationException.class, new Executable() {
+            
+            public void execute() throws Throwable {
+                tamanyoSer.addTamanyo(tamanyo);
+            }
+        });
+    }
 	//Se comprueba que los datos del tamanyo que se obtiene son los mismos que los datos del tamanyo recientemente añadida
 	@Test
 	public void findByIdTest() {
@@ -83,6 +106,18 @@ public class TamayoTestService {
 		Tamanyo tamanyoObtendio = tamanyoSer.findById(tamanyoAnadido.getId());
 		assertEquals(tamanyoObtendio.getId(), tamanyoAnadido.getId());
 	
+	}
+	//Se comprueba que si se le pasa una id erronea da un error de validacion
+	@Test
+	public void notFindByIdTest() {
+		tamanyo = new Tamanyo();
+		tamanyo.setCentro(centroAnadido);
+		tamanyo.setCoste(20.0);
+		tamanyo.setNombre(NombreTamanyo.M);
+		tamanyo.setSiguienteFase(Fases.disenyos);
+		tamanyo.setTiempo(30);
+		Tamanyo tamanyoAnanido = tamanyoSer.addTamanyo(tamanyo);
+		assertTrue(tamanyoSer.findById(tamanyoAnanido.getId()+1) == null);
 	}
 	//Se comprueba que una vez añadida el nuevo tamanyo esta se lista junto con las otras
 	@Test
@@ -152,5 +187,52 @@ public class TamayoTestService {
 		Tamanyo tamanyoAnadido = tamanyoSer.addTamanyo(tamanyo);
 		this.tamanyoSer.removeTamanyobyCentro(centroAnadido.getId());
 		assertFalse(tamanyoSer.findAll().contains(tamanyoAnadido));
+	}
+	//Se comprueba que la falta de datos provoca una excepcion a la hora de añadir una forma a un centro
+	@Test
+	public void addFormaCentroTest() {
+		Map<String, List<String>> datos = new HashMap<String, List<String>>();
+		String centroKey = "centro";
+		List<String> centroValue = new ArrayList<String>();
+		centroValue.add(centroAnadido.getId().toString());
+		String costeKey = "coste";
+		List<String> costeValue = new ArrayList<String>();
+		costeValue.add("50");
+		String duracionKey = "tiempo";
+		List<String> duracionValue = new ArrayList<String>();
+		duracionValue.add("5");
+		String persoKey = "personalizaciones";
+		List<String> persoValue = new ArrayList<String>();
+		persoValue.add("XXS");
+		datos.put(centroKey, centroValue); datos.put(costeKey, costeValue);datos.put(duracionKey, duracionValue); datos.put(persoKey, persoValue);
+		List<Tamanyo> formasAnadidas = tamanyoSer.addTamanyoCentro(datos);
+		assertEquals(centroSer.findById(centroAnadido.getId()).get(), formasAnadidas.get(0).getCentro());
+	}
+	//Se comprueba que la falta de datos provoca una excepcion a la hora de añadir una forma a un centro
+	@Test
+	public void addFormaCentroNullTest() {
+		Map<String, List<String>> datos = new HashMap<String, List<String>>();
+		String centroKey = "centro";
+		List<String> centroValue = null;
+		String costeKey = "coste";
+		List<String> costeValue = new ArrayList<String>();
+		costeValue.add("50");
+		String duracionKey = "tiempo";
+		List<String> duracionValue = new ArrayList<String>();
+		duracionValue.add("5");
+		String persoKey = "personalizaciones";
+		List<String> persoValue = new ArrayList<String>();
+		persoValue.add("XXS");
+		datos.put(centroKey, centroValue); datos.put(costeKey, costeValue);datos.put(duracionKey, duracionValue); datos.put(persoKey, persoValue); 
+		assertThrows(IllegalArgumentException.class, new Executable() {
+            
+            public void execute() throws Throwable {
+            	tamanyoSer.addTamanyoCentro(datos);
+            }
+        });
+	}
+	@AfterEach
+	public void deleteData() {
+		centroSer.delete(centroAnadido.getId());
 	}
 }
