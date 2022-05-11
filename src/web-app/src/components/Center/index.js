38acc0f34a203a37834875ceb8c3272baea119c6
Daniel-Caro <61906384+Daniel-Caro@users.noreply.github.com>
2022-05-11 16:43:32 +0200

    Información de dirección y localidad
    Se muestra al usuario la dirección y localidad de cada centro

--- src/web-app/src/components/Center/index.js
+++ src/web-app/src/components/Center/index.js
@@ -16,6 +16,7 @@ export function Center ({ center, isAdmin, detailsFunc, editFunc, deleteFunc }) 
                 </CardContent>
                 <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '49%'}}>
                     <p className="m-1 text-lg text-center"><strong>{center.nombre}</strong></p>
+                    <p className="m-1 text-lg text-center">{center.direccion}, {center.localidad}</p>
                     <button id="1" className="border-2 border-[#e0b4c7] bg-[#ffe2ef] text-lg text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-purple-300 hover:border-purple-300 transition duration-300"
                         onClick={detailsFunc}>Más detalles</button>
                 {isAdmin ?
@@ -43,6 +44,7 @@ export function Center ({ center, isAdmin, detailsFunc, editFunc, deleteFunc }) 
                 </CardContent>
                 <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '49%'}}>
                     <p className="m-1 text-lg text-center"><strong>{center.nombre}</strong></p>
+                    <p className="m-1 text-lg text-center">{center.direccion}, {center.localidad}</p>
                     <button id="1" className="border-2 border-[#e0b4c7] bg-[#ffe2ef] text-lg text-black w-auto py-3 m-3 rounded-md text-1xl font-medium hover:bg-purple-300 hover:border-purple-300 transition duration-300"
                         onClick={detailsFunc}>Más detalles</button>
                 {isAdmin ?
