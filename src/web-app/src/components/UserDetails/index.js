17459535cb458c4c4a2b1fe5c6989a5a9f5db205
Daniel-Caro <61906384+Daniel-Caro@users.noreply.github.com>
2022-05-14 09:47:17 +0200

    Eliminacion foto usuario
    Se ha eliminado la foto placeholder del usuario en la pestaña de "mi perfil"

--- src/web-app/src/components/UserDetails/index.js
+++ src/web-app/src/components/UserDetails/index.js
@@ -89,8 +89,6 @@ export function UserDetails({ image, email, phone }) {
     <Card style={{ backgroundColor: 'rgb(248, 225, 228)' }} sx={{ minWidth: 275 }}>
       <CardContent style={{marginBottom: "20px"}}>
         <div className="md:flex">
-          
-          <img src={image} alt="img" style={{"max-width": "100px", "max-height": "100px", "margin-right": "5px"}} className="rounded-md shadow-md w-full md:h-full md:w-1/4 float-left bg-white mb-2" />
           <div className="ml-5 items-center">
             <p><strong>Email:</strong> {email}</p>
             <p><strong>Teléfono:</strong> {phone}</p>
