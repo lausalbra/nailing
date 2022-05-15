2c116205867df36e9ae53124b6226a503ae75238
Antonio González Gómez <antoniogg696@gmail.com>
2022-05-15 16:16:06 +0200

    feat: Conectada vista a backend

--- src/web-app/src/components/CitaCentro/index.js
+++ src/web-app/src/components/CitaCentro/index.js
@@ -9,29 +9,33 @@ export function CitaCentro ({ cita }) {
         return date.toLocaleString("es-ES")
     }
 
+    function persParse(pers) {
+       return pers != null ? pers.nombre.replace(/_/g, " ") : 'Nada'
+    }
+
     return (
         <Box style={{background: 'linear-gradient(145deg, rgba(255,226,239,1) 5%, rgba(235,190,210,1) 100%)'}}
             sx={{ width: '88%', flexDirection: 'row', mx:'1%', my:2, display: 'inline-flex', alignItems: 'center', justifyContent: 'space-around', border:2, borderRadius: 2, borderColor: '#e0b4c7' }}>
-            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '24.5%'}}>
+            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '20.5%'}}>
                 <p className="m-2 text-lg text-center">{cita.usuario.usuario}</p>
                 <p className="m-2 text-lg text-center">{cita.usuario.email}</p>
                 <p className="m-2 text-lg text-center">{cita.usuario.telefono}</p>
             </Box>
-            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '24.5%'}}>
-                <p className="m-2 text-lg text-center">Inicio cita: {dateParse(cita.horaInicio)}</p>
-                <p className="m-2 text-lg text-center">Final Cita: {dateParse(cita.horaFin)}</p>
+            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '20.5%'}}>
+                <p className="m-2 text-lg text-center">{dateParse(cita.horaInicio).split(',')[0]}</p>
+                <p className="m-2 text-lg text-center">{dateParse(cita.horaInicio).split(',')[1].slice(0, -3)} - {dateParse(cita.horaFin).split(',')[1].slice(0, -3)}</p>
                 <p className="m-2 text-lg text-center">Precio: {cita.coste} €</p>
             </Box>
-            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '24.5%'}}>
-                <p className="m-2 text-lg text-center">Decoración: {cita.decoracion}</p>
-                <p className="m-2 text-lg text-center">Acabado: {cita.acabado}</p>
-                <p className="m-2 text-lg text-center">Base: {cita.base}</p>
-                <p className="m-2 text-lg text-center">Tipo: {cita.tipo}</p>
+            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '28.5%'}}>
+                <p className="m-2 text-lg text-center">Decoración: {persParse(cita.decoracion)}</p>
+                <p className="m-2 text-lg text-center">Acabado: {persParse(cita.acabado)}</p>
+                <p className="m-2 text-lg text-center">Base: {persParse(cita.base)}</p>
+                <p className="m-2 text-lg text-center">Tipo: {persParse(cita.tipo)}</p>
             </Box>
-            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '24.5%'}}>
-                <p className="m-2 text-lg text-center">Diseño: {cita.disenyo}</p>
-                <p className="m-2 text-lg text-center">Tamaño: {cita.tamanyo}</p>
-                <p className="m-2 text-lg text-center">Forma: {cita.forma}</p>
+            <Box disableSpacing sx={{flexDirection: 'column', display: 'inline-flex', width: '28.5%'}}>
+                <p className="m-2 text-lg text-center">Diseño: {persParse(cita.disenyo)}</p>
+                <p className="m-2 text-lg text-center">Tamaño: {persParse(cita.tamanyo)}</p>
+                <p className="m-2 text-lg text-center">Forma: {persParse(cita.forma)}</p>
             </Box>
         </Box>
     )
