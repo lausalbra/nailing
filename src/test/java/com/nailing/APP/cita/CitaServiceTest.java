d83f81b6d103dcc085ba03054b56e991821aca37
Jacgarvel <jacobo.estudiante16@gmail.com>
2022-05-17 21:44:48 +0200

    test: Avance en Coverage Se ha aumentado el coverage en los servicios de Centro, Cita y Tamaño. Issue #429

--- src/test/java/com/nailing/APP/cita/CitaServiceTest.java
+++ src/test/java/com/nailing/APP/cita/CitaServiceTest.java
@@ -14,7 +14,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -75,6 +78,33 @@ public class CitaServiceTest {
         assertEquals(ncn,nc+1);   
     }
     
+    @Test
+    public void ShouldNotAddCita(){
+        Integer nc = 0;
+        for (Iterator<Cita> it = citaService.findAll().iterator(); it.hasNext();) {
+            it.next();
+            nc++;
+        }
+        
+        Map<String,String> cita = new HashMap<>();
+        cita.put("usuario", null);
+        cita.put("centro", "1");
+        cita.put("precio", "50.2");
+        cita.put("tiempo", "25");
+        cita.put("tipo", "1");
+        cita.put("base", "1");
+        cita.put("forma", "1");
+        cita.put("tamanyo", "1");
+        cita.put("disenyo", "1");
+        cita.put("decoracion", "1");
+        cita.put("acabado", "1");
+        cita.put("fecha", "2032-05-22 10:30");
+        cita.put("disenyo", "1");
+        
+        
+        assertThrows(IllegalArgumentException.class, () -> citaService.addCita(cita));
+    }
+
     @Test
     public void shouldFindAll(){
         assertNotNull(citaService.findAll());
@@ -142,6 +172,31 @@ public class CitaServiceTest {
             List<String> disponibles = citaService.findDisponibles("2022-12-12 12",30,4L);
             assertTrue(disponibles!=null);
             assertTrue(!disponibles.isEmpty());
+    }
+
+    @Test
+    public void shouldRemoveCitaByUser(){
+        Map<String,String> cita = new HashMap<>();
+        cita.put("usuario", "1");
+        cita.put("centro", "1");
+        cita.put("precio", "50.2");
+        cita.put("tiempo", "25");
+        cita.put("tipo", "1");
+        cita.put("base", "1");
+        cita.put("forma", "1");
+        cita.put("tamanyo", "1");
+        cita.put("disenyo", "1");
+        cita.put("decoracion", "1");
+        cita.put("acabado", "1");
+        cita.put("fecha", "2032-05-22 10:30");
+        cita.put("disenyo", "1");
+        
+        Cita citaResult = citaService.addCita(cita);
+
+        List<Cita> listaCon = citaService.findByUsuario(1L);
+        citaService.removeCitaByUser(1L, citaResult.getId());
+        List<Cita> listaSin = citaService.findByUsuario(1L);
+        assertNotEquals(listaCon, listaSin);
     }
     
 }
