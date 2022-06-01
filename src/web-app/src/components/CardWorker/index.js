67949e693339eff658eec1d9d9e09df68c79403f
Daniel-Caro <61906384+Daniel-Caro@users.noreply.github.com>
2022-06-01 17:03:55 +0200

    Arreglos responsive
    Se han modificado algunas propiedades de ciertos elementos que no eran completamente responsive

--- src/web-app/src/components/CardWorker/index.js
+++ src/web-app/src/components/CardWorker/index.js
@@ -1,7 +1,7 @@
 export function CardWorker({ photo, name, role, mail }) {
     return (
         <>
-            <div className="border-2 border-pink-300 m-5 p-3 pb-1 h-80 w-44 rounded-md ">
+            <div className="border-2 border-pink-300 m-4 p-3 pb-1 h-80 w-44 rounded-md ">
                 <img className="m-auto h-[175px] w-[120px]" src={photo} alt="Cara"></img>
                 <p className="border-t-4 border-t-pink-300"><strong>{name}</strong></p>
                 <p>{role}</p>
